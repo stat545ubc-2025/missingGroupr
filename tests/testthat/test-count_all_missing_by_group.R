4cad5ed60b3494666f8a407b2b3b04c3cf2ad7c1
sasha-s12 <sasha.sharma@stat.ubc.ca>
2025-11-18 14:47:30 -0800

    final

--- tests/testthat/test-count_all_missing_by_group.R
+++ tests/testthat/test-count_all_missing_by_group.R
@@ -1,3 +1,33 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(testthat)
+library(dplyr)
+library(tibble)
+
+test_that("Output matches direct call to dplyr", {
+  small_tbl <- tibble::tribble(~group, ~var1, ~var2,
+                       "A", 1, NA,
+                       "A", 2, "x",
+                       "B", NA, "y",
+                       "C", 3, "z")
+
+  expect_equal(small_tbl |>  dplyr::group_by(group) |>
+                  dplyr::summarize(dplyr::across(dplyr::everything(), ~sum(is.na(.x))),
+                            .groups = "drop"),
+                count_all_missing_by_group(small_tbl, group)
+  )
+
+  expect_equal( small_tbl |>  dplyr::group_by(group) |>
+                  dplyr::summarize( dplyr::across( dplyr::everything(), ~sum(is.na(.x))),
+                            .groups = NULL),
+                count_all_missing_by_group(small_tbl, group, NULL)
+  )
 })
+
+test_that("Checking error handling for .groups input", {
+  expect_error(
+    count_all_missing_by_group(airquality, Month, "kep")
+  )
+  expect_no_error(
+    count_all_missing_by_group(airquality, Month, NULL)
+  )
+})
+
